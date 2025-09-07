76c71d90a90a29147b25425d7ac7d7dddbe1c707
Stéphane Erard <stephane.erard@gmail.com>
2025-09-08 00:28:22 +0200

    wip

--- CSharp.Object.Model/src/FrenchExDev.Net.CSharp.Object.Model.Abstractions/IConverter.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FrenchExDev.Net.CSharp.Object.Model.Abstractions;
-
-public interface IConverter<TFrom, TTo>
-{
-    TTo Convert(TFrom from);
-}
-
--- Dotnet/Project/test/FrenchExDev.Net.Dotnet.Project.Tests/UnitTest1.cs
+++ Dotnet/Project/test/FrenchExDev.Net.Dotnet.Project.Tests/UnitTest1.cs
@@ -1,11 +1,38 @@
-﻿namespace FrenchExDev.Net.Dotnet.Project.Tests
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+﻿using FrenchExDev.Net.CSharp.Object.Builder.Abstractions;
+using FrenchExDev.Net.CSharp.Object.Model.Abstractions;
+using FrenchExDev.Net.CSharp.Object.Model.Infrastructure;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 
-        }
+namespace FrenchExDev.Net.Dotnet.Project.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        var compilationUnit = SyntaxFactory.CompilationUnit();
+
+        NamespaceDeclarationModel myModel = new NamespaceDeclarationModelBuilder()
+            .Name("MyNamespace")
+            .Interface(b => b
+                .Name("IMyInterface")
+                .Modifier(InterfaceModifier.Public)
+                .Method(m => m
+                    .Name("MyMethod")
+                    .ReturnType("string")
+                    .Parameter((b) => b.Name("MyParameter").Type("string").Build())
+                ))
+                .Build().Success();
+
+        var converter = new ModelRoslynConverter();
+
+        var myModelSyntax = converter.ToSyntax(myModel);
+
+        var code = SyntaxFactory.CompilationUnit()
+            .AddMembers(myModelSyntax)
+            .NormalizeWhitespace()
+            .ToFullString();
+
     }
 }
