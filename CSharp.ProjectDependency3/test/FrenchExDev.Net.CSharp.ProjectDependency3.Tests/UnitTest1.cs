5160d33dc37918935c3237703a6336db62976b40
Stéphane Erard <stephane.erard@gmail.com>
2025-10-24 21:53:12 +0200

    wip

--- CSharp.ProjectDependency3/test/FrenchExDev.Net.CSharp.ProjectDependency3.Tests/UnitTest1.cs
+++ CSharp.ProjectDependency3/test/FrenchExDev.Net.CSharp.ProjectDependency3.Tests/UnitTest1.cs
@@ -17,53 +17,53 @@ public class UnitTest1
         var dep3Sln = @"C:\code\FrenchExDev.Net\FrenchExDev.Net_i2\FrenchExDev.Net\CSharp.ProjectDependency3\CSharp.ProjectDependency3.sln";
         //var rootSln = @"C:\\code\\FrenchExDev.Net\\FrenchExDev.Net_i2\\FrenchExDev.Net\\Alpine\\FrenchExDev.Net.Alpine.sln";
         var playground = @"C:\code\FrenchExDev.Net\FrenchExDev.Net_i2\FrenchExDev.Net\CSharp.ProjectDependency3\test\playground";
-        //var msBuildRegisteringService = new MsBuildRegisteringService();
-        //msBuildRegisteringService.RegisterIfNeeded();
+        var msBuildRegisteringService = new MsBuildRegisteringService();
+        msBuildRegisteringService.RegisterIfNeeded();
 
-        //var defaultProjectCollection = new DefaultProjectCollection();
+        var defaultProjectCollection = new DefaultProjectCollection();
 
-        //var msBuildWorkspace = new MsBuildWorkspace(defaultProjectCollection, new NullLogger<IMsBuildWorkspace>());
-        //msBuildWorkspace.Initialize();
+        var msBuildWorkspace = new MsBuildWorkspace(defaultProjectCollection, new NullLogger<IMsBuildWorkspace>());
+        msBuildWorkspace.Initialize();
 
-        //var solutionR = await msBuildWorkspace.OpenSolutionAsync(rootSln);
-        //solutionR.IsSuccess.ShouldBeTrue();
+        var solutionR = await msBuildWorkspace.OpenSolutionAsync(rootSln);
+        solutionR.IsSuccess.ShouldBeTrue();
 
-        //var solution = solutionR.ObjectOrThrow();
+        var solution = solutionR.ObjectOrThrow();
 
-        //// Run analyzers
-        //var aggregator = new ProjectAnalyzerAggregator()
-        //    /* .Add(new StructuralCouplingAnalyzer())
-        //     .Add(new ClassicalCouplingAnalyzer())
-        //     .Add(new DirectionalCouplingAnalyzer())*/
-        //    .Add(new CodeGraphAnalyzer());
+        // Run analyzers
+        var aggregator = new ProjectAnalyzerAggregator()
+            /* .Add(new StructuralCouplingAnalyzer())
+             .Add(new ClassicalCouplingAnalyzer())
+             .Add(new DirectionalCouplingAnalyzer())*/
+            .Add(new CodeGraphAnalyzer());
 
-        //var results = await aggregator.RunAsync(solution);
-        //results.ShouldNotBeNull();
+        var results = await aggregator.RunAsync(solution);
+        results.ShouldNotBeNull();
 
-        ////// Generate markdown for each known analyzer result
-        //var doc = new MarkdownDocument();
-        //foreach (var kv in results)
-        //{
-        //    switch (kv.Value)
-        //    {
-        //        case StructuralCouplingResult sr:
-        //            var sSections = new StructuralCouplingReportGenerator().Generate(sr);
-        //            foreach (var s in sSections) doc.AddSection(s);
-        //            break;
-        //        case ClassicalCouplingResult cr:
-        //            var cSections = new ClassicalCouplingReportGenerator().Generate(cr);
-        //            foreach (var s in cSections) doc.AddSection(s);
-        //            break;
-        //        case DirectionalCouplingResult dr:
-        //            var dSections = new DirectionalCouplingReportGenerator().Generate(dr);
-        //            foreach (var s in dSections) doc.AddSection(s);
-        //            break;
-        //        case CodeGraphResult gr:
-        //            var json = JsonSerializer.Serialize(gr.Model, new JsonSerializerOptions { WriteIndented = true });
-        //            await File.WriteAllTextAsync(Path.Combine(playground, "graph.json"), json);
-        //            break;
-        //    }
-        //}
+        //// Generate markdown for each known analyzer result
+        var doc = new MarkdownDocument();
+        foreach (var kv in results)
+        {
+            switch (kv.Value)
+            {
+                case StructuralCouplingResult sr:
+                    var sSections = new StructuralCouplingReportGenerator().Generate(sr);
+                    foreach (var s in sSections) doc.AddSection(s);
+                    break;
+                case ClassicalCouplingResult cr:
+                    var cSections = new ClassicalCouplingReportGenerator().Generate(cr);
+                    foreach (var s in cSections) doc.AddSection(s);
+                    break;
+                case DirectionalCouplingResult dr:
+                    var dSections = new DirectionalCouplingReportGenerator().Generate(dr);
+                    foreach (var s in dSections) doc.AddSection(s);
+                    break;
+                case CodeGraphResult gr:
+                    var json = JsonSerializer.Serialize(gr.Model, new JsonSerializerOptions { WriteIndented = true });
+                    await File.WriteAllTextAsync(Path.Combine(playground, "graph.json"), json);
+                    break;
+            }
+        }
         //var markdown = doc.Render();
 
         //await File.WriteAllTextAsync(Path.Combine(playground, "output.md"), markdown);
