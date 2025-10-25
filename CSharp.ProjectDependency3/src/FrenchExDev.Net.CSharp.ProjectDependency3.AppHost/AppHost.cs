14c1a43b56bb353701d5aac839a341afeb809958
Stéphane Erard <stephane.erard@gmail.com>
2025-10-26 00:18:13 +0200

    fix

--- CSharp.ProjectDependency3/src/FrenchExDev.Net.CSharp.ProjectDependency3.AppHost/AppHost.cs
+++ CSharp.ProjectDependency3/src/FrenchExDev.Net.CSharp.ProjectDependency3.AppHost/AppHost.cs
@@ -1,16 +1,13 @@
 ﻿using FrenchExDev.Net.CSharp.ProjectDependency3.AppHost;
 using Microsoft.Extensions.Configuration;
 using Projects;
-using Aspire.Hosting;
 
 var builder = DistributedApplication.CreateBuilder(args);
 var dnsConfig = builder.Configuration.GetSection("DnsConfiguration").Get<DnsConfiguration>() ?? new DnsConfiguration();
 builder.EnsureSetup(dnsConfig);
 
-// Configure Aspire Dashboard with custom DNS
 var dashboardUrl = dnsConfig.GetDashboardUrl();
 Console.WriteLine($"Aspire Dashboard URL: {dashboardUrl}");
-
 
 var orchestratorUrl = dnsConfig.GetOrchestratorUrl();
 var orchestrator = builder.AddProject<FrenchExDev_Net_CSharp_ProjectDependency3_Worker_Orchestrator>("orchestrator")
@@ -26,7 +23,10 @@ var viz = builder.AddProject<FrenchExDev_Net_CSharp_ProjectDependency3_Viz>("viz
     .WithEnvironment("ASPNETCORE_URLS", vizUrl)
     .WithEnvironment("ASPNETCORE_Kestrel__Certificates__Default__Path", dnsConfig.CertPathOrDie())
     .WithEnvironment("ASPNETCORE_Kestrel__Certificates__Default__KeyPath", dnsConfig.KeyPathOrDie())
+    .WithEnvironment("OrchestratorUrl", orchestratorUrl)
     .WithReference(orchestrator);
+
+Console.WriteLine($"Using Viz URL: {vizUrl}");
 
 // Add worker agents
 var workers = new List<IResourceBuilder<ProjectResource>>();
@@ -34,6 +34,7 @@ var workers = new List<IResourceBuilder<ProjectResource>>();
 for (int i = 1; i <= dnsConfig.WorkerCount; i++)
 {
     var workerUrl = dnsConfig.GetWorkerUrl(i);
+    var workerPort = dnsConfig.Ports.WorkerBase + i - 1;
 
     var worker = builder.AddProject<FrenchExDev_Net_CSharp_ProjectDependency3_Worker_Agent>($"worker-agent-{i}")
         .WithEnvironment("ASPNETCORE_URLS", workerUrl)
@@ -41,6 +42,7 @@ for (int i = 1; i <= dnsConfig.WorkerCount; i++)
         .WithEnvironment("ASPNETCORE_Kestrel__Certificates__Default__KeyPath", dnsConfig.KeyPathOrDie())
         ;
 
+    Console.WriteLine($"Using Worker {i} URL: {workerUrl}");
     workers.Add(worker);
 }
 
